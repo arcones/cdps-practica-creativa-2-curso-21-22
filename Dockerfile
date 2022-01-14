6f14a94be1d61a1412997c219b6eb7c1c8ae795c
Marta Arcones Rodríguez <marta.arcones@gmail.com>
2022-01-14 21:09:00 +0100

    WIP

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM python:3.9.9
+
+# I will use this dockerfile to do integration test with the script
--- deployInGCP.py
+++ deployInGCP.py
@@ -1,7 +1,28 @@
 #!/usr/bin/python3
 
 from git import Repo
+import subprocess
+import sys
+import shutil
 
 URL = "https://github.com/CDPS-ETSIT/practica_creativa2.git"
+REPO_PATH = ".\\app"
 
-Repo.clone_from(URL, "app")
+
+def _cleanup_from_previous_executions ():
+    try:
+        shutil.rmtree(REPO_PATH)
+    except Exception as e:
+        print(f"Error deleting directoy: {e}")
+
+def _clone_repo_and_go():
+    Repo.clone_from(URL, REPO_PATH)
+    shutil.move(".", f"./{REPO_PATH}/bookinfo/src/productpage")
+
+def _install_deps():
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])
+
+if __name__=="__main__":
+    _cleanup_from_previous_executions()
+    _clone_repo_and_go()
+    _install_deps()
