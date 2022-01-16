4ffa14febdb9dbd360e50a354e7b145059315582
Marta Arcones <marta.arconesrodriguez@zooplus.com>
2022-01-16 19:47:17 +0100

    Format code

--- setupNLaunchBookStore.py
+++ setupNLaunchBookStore.py
@@ -17,62 +17,63 @@ REPO_PATH = f"{os.getcwd()}/app"
 
 
 def _cleanup_from_previous_executions():
-    subprocess.check_call(['rm', '-rf', REPO_PATH])
-    subprocess.Popen(['sudo', 'fuser', '-k', '80/tcp', f'{PORT}/tcp'])
+  subprocess.check_call(['rm', '-rf', REPO_PATH])
+  subprocess.Popen(['sudo', 'fuser', '-k', '80/tcp', f'{PORT}/tcp'])
 
 
 def _clone_repo_and_go():
-    print(REPO_PATH)
-    Repo.clone_from(URL, REPO_PATH)
-    os.chdir(f"{REPO_PATH}/bookinfo/src/productpage")
+  print(REPO_PATH)
+  Repo.clone_from(URL, REPO_PATH)
+  os.chdir(f"{REPO_PATH}/bookinfo/src/productpage")
+
 
 def _install_deps():
-    try:
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])
-    except:
-        pass
+  try:
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])
+  except:
+    pass
+
 
 def _fix_deps():
-    subprocess.check_call(['pip', 'uninstall', '-y', 'urllib3', 'jsonchema'])
-    subprocess.check_call(['pip', 'install', 'urllib3==1.24.1', 'jsonschema==2.6.0'])
-    subprocess.check_call(['pip', 'install', '--upgrade', 'requests'])
+  subprocess.check_call(['pip', 'uninstall', '-y', 'urllib3', 'jsonchema'])
+  subprocess.check_call(['pip', 'install', 'urllib3==1.24.1', 'jsonschema==2.6.0'])
+  subprocess.check_call(['pip', 'install', '--upgrade', 'requests'])
+
 
 def _set_website_title():
-    if not 'GROUP_NUMBER' in os.environ:
-        os.environ["GROUP_NUMBER"] = "Equipo 09"
+  if not 'GROUP_NUMBER' in os.environ:
+    os.environ["GROUP_NUMBER"] = "Equipo 09"
 
-    search_text = "BookInfo Sample"
-    replace_text = f"BookInfo Sample {os.environ['GROUP_NUMBER']}"
+  search_text = "BookInfo Sample"
+  replace_text = f"BookInfo Sample {os.environ['GROUP_NUMBER']}"
 
-    with open(r"./templates/productpage.html", "r+" ) as productpage_source:
-        data = productpage_source.read()
-        data = data.replace(search_text, replace_text)
-  
-    with open(r"./templates/productpage.html", 'w') as productpage_source:
-        productpage_source.write(data)
+  with open(r"./templates/productpage.html", "r+") as productpage_source:
+    data = productpage_source.read()
+    data = data.replace(search_text, replace_text)
+
+  with open(r"./templates/productpage.html", 'w') as productpage_source:
+    productpage_source.write(data)
+
 
 def _port_forwarding_setup_n_information():
-    subprocess.Popen(['sudo', 'socat', 'tcp-listen:80,reuseaddr,fork', f'tcp:localhost:{PORT}'],
-                          stdout=subprocess.DEVNULL,
-                          stderr=subprocess.STDOUT)
+  subprocess.Popen(['sudo', 'socat', 'tcp-listen:80,reuseaddr,fork', f'tcp:localhost:{PORT}'], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
-    subprocess.check_call(['figlet', '\n\nbookstore app ready'])
-    public_ip = subprocess.check_output(["curl", "-L", "ifconfig.me"]).decode("utf-8")
+  subprocess.check_call(['figlet', '\n\nbookstore app ready'])
+  public_ip = subprocess.check_output(["curl", "-L", "ifconfig.me"]).decode("utf-8")
 
-    print(
-        f"\nAcceda a la aplicación en http://{public_ip}/productpage\n\n\n\n")
+  print(f"\nAcceda a la aplicación en http://{public_ip}/productpage\n\n\n\n")
 
 
 def _run_app():
-    subprocess.call([sys.executable, 'productpage_monolith.py', f"{PORT}"])
+  subprocess.call([sys.executable, 'productpage_monolith.py', f"{PORT}"])
 
 
 if __name__ == "__main__":
-    _cleanup_from_previous_executions()
-    _clone_repo_and_go()
-    _install_deps()
-    _fix_deps()
-    _set_website_title()
-    _port_forwarding_setup_n_information()
-    _run_app()
-    _set_website_title()
+  _cleanup_from_previous_executions()
+  _clone_repo_and_go()
+  _install_deps()
+  _fix_deps()
+  _set_website_title()
+  _port_forwarding_setup_n_information()
+  _run_app()
+  _set_website_title()
